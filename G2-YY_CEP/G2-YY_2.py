232473e97cd7266a14b44b0555eba53145fc691d
Usman Rasheed Siddiqui <siddiqui.uas1@gmail.com>
2025-03-21 11:35:14 +0500

    Changings

--- G2-YY_CEP/G2-YY_2.py
+++ G2-YY_CEP/G2-YY_2.py
@@ -4,8 +4,7 @@ import os
 class Cars:
     '''A Class of Cars Used to check on the number of cars available for rent and their brand, models
     and seating capacity.'''
-    def __init__(self, cars=[], brand="", model="", seating_capacity=0, price_per_day=0,no_of_cars=0, available=False):
-        self.cars = cars
+    def __init__(self, brand="", model="", seating_capacity=0, price_per_day=0,no_of_cars=0, available=False):
         self.brand = brand
         self.seating_capacity = seating_capacity
         self.model = model
@@ -19,18 +18,9 @@ class Cars:
 
     def display_car_info(self):
         '''To read the information of the car'''
-        self.file_name = f"Car/{self.brand}_{self.model}.txt"
-        self.car_info = f'''
-        ================================
-                CAR INFORMATION
-        ================================
-        Car Name: {self.brand}
-        Model Name: {self.model}
-        Seating Capacity: {self.seating_capacity}
-        Price Per Day: {self.price_per_day}
-        No of Cars: {self.no_of_cars}
-        Avalaibility: {"Available" if self.available else "Not Available"}
-        '''
+        self.get_file_name()
+        self.set_car_info()
+
         if os.path.exists(self.file_name):
             with open(self.file_name, "r") as file:
                 content = file.read()
@@ -41,7 +31,7 @@ class Cars:
     def get_file_name(self):
         self.file_name = f"Car/{self.brand}_{self.model}.txt"
 
-    def get_car_info(self):
+    def set_car_info(self):
         self.car_info = f'''
                 ================================
                         CAR INFORMATION
@@ -53,6 +43,9 @@ class Cars:
                 No of Cars: {self.no_of_cars}
                 Avalaibility: {"Available" if self.available else "Not Available"}
                 '''
+    def get_car_info(self):
+        return self.car_info
+
 
     def rent_car(self):
         '''To update the rent record of a car'''
@@ -63,15 +56,15 @@ class Cars:
 
     def return_car(self):
         '''To update the rent record of a car'''
-        self.avalaible = True
+        self.available = True
 
     def __str__(self):
-        return f"{self.display_car_info() if self.available else None}"
+        return f"{self.display_car_info() if self.available else "Car not Available"}"
 
 class Administrator(Cars):
 
     def __init__(self):
-        Cars.__init__(self, cars=[], brand="", model="", seating_capacity=0, price_per_day=0, no_of_cars=0, available=True)
+        Cars.__init__(self,  brand="", model="", seating_capacity=0, price_per_day=0, no_of_cars=0, available=True)
 
     def update_car(self):
         '''To Update or Add the record of car'''
@@ -91,9 +84,7 @@ class Administrator(Cars):
         self.available = True
         self.update_car()
 
-C1 = Cars()
-C1.display_car_info()
-
-
-
-
+C1 = Cars(input("Enter Brand Name: "), input("Enter Model Name: "), int(input("Enter Seating Capacity: ")),
+              int(input("Enter Price Per Day: ")), int(input("Enter No of Cars: ")), input("Enter Availability (True/False): ").strip().lower() == "true")
+C1.set_car_info()
+print(C1.display_car_info())
